16ab59134964e554a1a10f06635c0c6fd6dc5fba
TUAN-ANH <72487483+TUANANH-INU@users.noreply.github.com>
2022-08-08 14:13:13 +0700

    Update port_android.py

--- port_android.py
+++ port_android.py
@@ -4,7 +4,19 @@ from torch.utils.mobile_optimizer import optimize_for_mobile
 
 model = torchvision.models.mobilenet_v2(pretrained=True)
 model.eval()
+
+# only one input
 example = torch.rand(1, 3, 224, 224)
+
+# multiple inputs
+example_1 = torch.rand(1, 3, 224, 224)
+example_2 = torch.rand(1, 10, 2, 2)
+example_3 = torch.rand(1, 10, 2)
+example = (example_1, example_2, example_3)
+
+# convert
 traced_script_module = torch.jit.trace(model, example)
 traced_script_module_optimized = optimize_for_mobile(traced_script_module)
 traced_script_module_optimized._save_for_lite_interpreter("android.ptl")
+
+
